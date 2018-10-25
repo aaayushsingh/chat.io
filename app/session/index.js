30d1fe1531138253958ef4a84406e01d491a8d78
Ayush Singh <ayush-_-singh@live.com>
2018-10-26 02:11:04 +0530

    always use redis for session store move nodemon to dev dependency

--- app/session/index.js
+++ app/session/index.js
@@ -1,32 +1,44 @@
-'use strict';
+// @ts-check
+"use strict";
 
-var session 	= require('express-session');
-var MongoStore	= require('connect-mongo')(session);
-var db 		    = require('../database');
-var config 		= require('../config');
+var session = require("express-session");
+var RedisStore = require("connect-redis")(session);
+var db = require("../database");
+var config = require("../config");
 
 /**
  * Initialize Session
  * Uses MongoDB-based session store
  *
  */
-var init = function () {
-	if(process.env.NODE_ENV === 'production') {
-		return session({
-			secret: config.sessionSecret,
-			resave: false,
-			saveUninitialized: false,
-			unset: 'destroy',
-			store: new MongoStore({ mongooseConnection: db.Mongoose.connection })
-		});
-	} else {
-		return session({
-			secret: config.sessionSecret,
-			resave: false,
-			unset: 'destroy',
-			saveUninitialized: true
-		});
-	}
-}
+var init = function() {
+  if (process.env.NODE_ENV === "production") {
+    return session({
+      secret: config.sessionSecret,
+      resave: false,
+      saveUninitialized: false,
+      unset: "destroy",
+      store: new RedisStore({
+        host: "127.0.0.1",
+        port: 6379,
+        prefix: "sess_",
+        pass: ""
+      })
+    });
+  } else {
+    return session({
+      secret: config.sessionSecret,
+      resave: false,
+      unset: "destroy",
+      saveUninitialized: true,
+      store: new RedisStore({
+        host: "127.0.0.1",
+        port: 6379,
+        prefix: "sess_",
+        pass: ""
+      })
+    });
+  }
+};
 
 module.exports = init();
