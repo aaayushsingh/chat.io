03b3dde9eaaefe9ac51b2c56fd74b694f8f22e8a
Ayush Singh <ayush-_-singh@live.com>
2018-10-26 02:15:18 +0530

    use redis for prod and dev

--- app/session/index.js
+++ app/session/index.js
@@ -12,33 +12,18 @@ var config = require("../config");
  *
  */
 var init = function() {
-  if (process.env.NODE_ENV === "production") {
-    return session({
-      secret: config.sessionSecret,
-      resave: false,
-      saveUninitialized: false,
-      unset: "destroy",
-      store: new RedisStore({
-        host: "127.0.0.1",
-        port: 6379,
-        prefix: "sess_",
-        pass: ""
-      })
-    });
-  } else {
-    return session({
-      secret: config.sessionSecret,
-      resave: false,
-      unset: "destroy",
-      saveUninitialized: true,
-      store: new RedisStore({
-        host: "127.0.0.1",
-        port: 6379,
-        prefix: "sess_",
-        pass: ""
-      })
-    });
-  }
+  return session({
+    secret: config.sessionSecret,
+    resave: false,
+    saveUninitialized: false,
+    unset: "destroy",
+    store: new RedisStore({
+      host: "127.0.0.1",
+      port: 6379,
+      prefix: "sess_",
+      pass: ""
+    })
+  });
 };
 
 module.exports = init();
