0d63b5fd7f100fc9eacaaa72334f931cf7b703b0
Ayush Singh <ayush-_-singh@live.com>
2018-10-26 04:11:06 +0530

    update user model

--- app/models/user.js
+++ app/models/user.js
@@ -16,32 +16,6 @@ var findById = function(id, callback) {
 };
 
 /**
- * Find a user, and create one if doesn't exist already.
- * This method is used ONLY to find user accounts registered via Social Authentication.
- *
- */
-var findOrCreate = function(data, callback) {
-  findOne({ socialId: data.id }, function(err, user) {
-    if (err) {
-      return callback(err);
-    }
-    if (user) {
-      return callback(err, user);
-    } else {
-      var userData = {
-        username: data.displayName,
-        socialId: data.id,
-        picture: data.photos[0].value || null
-      };
-
-      create(userData, function(err, newUser) {
-        callback(err, newUser);
-      });
-    }
-  });
-};
-
-/**
  * A middleware allows user to get access to pages ONLY if the user is already logged in.
  *
  */
@@ -57,6 +31,5 @@ module.exports = {
   create,
   findOne,
   findById,
-  findOrCreate,
   isAuthenticated
 };
